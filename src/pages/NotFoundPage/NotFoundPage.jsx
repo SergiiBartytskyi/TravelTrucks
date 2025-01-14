11d8b663158f43dff1450204c9fba667769ed950
SergiiBartytskyi <s.bartycjkyj@gmail.com>
2025-01-14 15:54:03 +0100

    add NotFoundPage

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,11 +1,15 @@
 import { Link } from "react-router";
+import { RiArrowGoBackLine } from "react-icons/ri";
 import styles from "./NotFoundPage.module.css";
 
 const NotFoundPage = () => {
   return (
     <div className={styles.container}>
-      <Link to="/">Back to Home</Link>
-      <h1>404 | Page not found!</h1>
+      <Link to="/" className={styles.link}>
+        <RiArrowGoBackLine />
+        Back to Home
+      </Link>
+      <h1 className={styles.title}>404 | Page not found!</h1>
     </div>
   );
 };
